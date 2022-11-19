159d43cdb0eb9499a8b1bff84748aa1d91f2b0d7
shoham <shohamweiss@gmail.com>
2022-11-19 16:49:18 -0600

    Adding all ui interaction components

---
+++ options.js
@@ -0,0 +1,18 @@
+options = {
+    "Greetings": {
+        "Hello": "Hello"
+    },
+    "Folder2": {
+        "SubFolder1": {
+            "Test": "Test"
+        }
+    }
+    ,
+    "Folder3": {
+        "Hello": "Hello"
+    }
+    ,
+    "Folder4": {
+        "Hello": "Hello"
+    }
+}
