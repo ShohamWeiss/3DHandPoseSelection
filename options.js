9e02dd745a3fe268ef56aa63d2bfac63d7ae61bc
Jazzmin Casillas <jazzmincasillas@gmail.com>
2022-11-30 20:26:16 -0600

    finish options

--- options.js
+++ options.js
@@ -7,7 +7,7 @@ options = {
         "Have a nice day!" : "Have a nice day!",
         "How are you?" : "How are you?"
     },
-    "Who/What/When/Where?": {
+    "Questions": {
         "Who?": {
             "Who?": "Who?",
             "Who are you?" : "Who are you?",
@@ -21,8 +21,8 @@ options = {
             "What are you?" : "What are you?",
             "What happened?" : "What happened?",
             "What is that?" : "What is that?",
-            "What ?" : "What ?",
-            "What ?" : "What ?"
+            "What did you do?" : "What did you do?",
+            "What are we waiting for?" : "What are we waiting for?"
         },
         "When?" : {
             "When?" : "When?",
@@ -30,34 +30,84 @@ options = {
             "When are we?" : "When are we?",
             "When is he/she?" : "When is he/she?",
             "When are they?" : "When are they?",
-            "When ?" : "When ?"
+            "When can we?" : "When can we?"
         },
         "Where?" : {
             "Where?" : "Where?",
             "Where is it?" : "Where is it?",
-            "Where ?" : "Where ?",
-            "Where ?" : "Where ?",
-            "Where ?" : "Where ?",
-            "Where ?" : "Where ?"
-        }
-    },
-    "Why/How?/Phrases": {
+            "Where are you?" : "Where are you?",
+            "Where did you go?" : "Where did you go?",
+            "Where are we going?" : "Where are we going?",
+            "Where can I?" : "Where can I?"
+        },
         "Why?" : {
             "Why?" : "Why?",
-            "Why ?" :"Why ?",
+            "Why is that?" : "Why is that?",
+            "Why should we?" : "Why should we?",
+            "Why does that work?" : "Why does that work?",
+            "Why do they?" : "Why do they?",
+            "Why did that?" : "Why did that?" 
         },
-        "How?" : {
+        "How?" :{
             "How?" : "How?",
-
+            "How are you?" : "How are you?",
+            "How is the weather?" : "How is the weather?",
+            "How can I?" : "How can I?",
+            "How did it happen?" : "How did it happen?",
+            "How is it?" : "How is it?",
+        }
+    },
+    "Numbers": {
+        "25s" : {
+            "25" : "25",
+            "50" :"50",
+            "75" : "75",
+            "100" : "100",
+            "150" : "150",
+            "200" : "200"
+        },
+        "1s" : {
+            "1" : "1",
+            "2" : "2",
+            "3" : "3",
+            "4" : "4",
+            "5" : "5",
+            "6" : "6",
+            "7" : "7",
+            "8" : "8",
+            "9" : "9",
+        },
+        "10s" : {
+            "10" : "10",
+            "20" : "20",
+            "30" : "30",
+            "40" : "40",
+            "50" : "50",
+            "60" : "60",
+            "70" : "70",
+            "80" : "80",
+            "90" : "90",
+        },
+        "100s" : {
+            "100" : "100",
+            "200" : "200",
+            "300" : "300",
+            "400" : "400",
+            "500" : "500",
+            "600" : "600",
+            "700" : "700",
+            "800" : "800",
+            "900" : "900",
+            "1000" : "1000",
         }
     }
     ,
     "Daily Activites": {
         "Eat": "Eat",
         "Sleep" : "Sleep",
-        "" : "",
-        "" : "",
-        "" : "",
-        "" : ""
+        "Lunch/Dinner" : "Lunch/Dinner",
+        "Breakfast" : "Breakfast",
+        "Work" : "Work",
+        "Study" : "Study"
     }
 }
